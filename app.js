897cc3509532391ed019498d87417e0d5512062c
Your Name <you@example.com>
2022-07-14 19:53:18 -0600

    Finished another KC'

--- app.js
+++ app.js
@@ -15,3 +15,17 @@ let moreClassTags = document.getElementsByClassName(`lol`);
 for (let i = 0; i < moreClassTags.length; i++) {
     moreClassTags[i][`innerText`] = `grabbed by my class, differently`;
     }
+
+    let newh1 = document.getElementById(`divH1`);
+    newh1[`innerHTML`] = `aha, it no says what it said before`
+
+    let newer = document.getElementById(`divH1`);
+    newer[`outerHTML`] = `<h1>Woah, what happened?<h1>` + newer[`outerHTML`];
+
+    let newest = document.getElementById(`divH1`);
+    newest[`outerHTML`] = newest[`outerHTML`] + `<h1>HERE WE ARE<h1>`;
+
+    let classtags = document.querySelectorAll(`.dawn`);
+    for (let i = 0; i < classtags.length; i++) {
+        classtags[i][`innerText`] = `hacked`;
+        }
