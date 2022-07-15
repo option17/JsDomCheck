c51c077460b8875d33192016a7b150fcd0f661f7
Your Name <you@example.com>
2022-07-14 18:58:04 -0600

    finished another KC

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ let classTags = document.querySelectorAll(`.shambala`);
 for (let i = 0; i < classTags.length; i++) {
 classTags[i][`innerText`] = `grabbed by my class`;
 }
+
+let moreIdTags = document.getElementById(`here`);
+moreIdTags[`innerText`] = `Another ID TAG CRAZY`;
+
+let moreClassTags = document.getElementsByClassName(`lol`);
+for (let i = 0; i < moreClassTags.length; i++) {
+    moreClassTags[i][`innerText`] = `grabbed by my class, differently`;
+    }
